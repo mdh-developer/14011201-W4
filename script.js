0f67590ea68d8057faae29de760a1479abdfd3e6
mohammadid4472 <106327105+mohammadid4472@users.noreply.github.com>
2022-05-27 19:29:29 +0430

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+const month = "8";
+
+switch (month) {
+    case "1":
+    case "2":
+    case "3":
+      console.log("Spring");
+      break;
+    case "4":
+    case "5":
+    case "6":
+      console.log("Summer");
+      break;
+    case "7":
+    case "8":
+    case "9":
+      console.log("Fall");
+      break;
+    case "10":
+    case "11":
+    case "12":
+      console.log("Winter");
+      break;
+    default:
+      console.log("not a valid day");
+}
